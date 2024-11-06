1149137d62fa8034be43b910fc46fe2ee8d1b927
lorenqg <lorenquiceno@gmail.com>
2024-11-06 18:45:36 -0500

    docs: update method `sendDestinity` comment for better clarity and usage example

--- src/app/servicios/destino.service.ts
+++ src/app/servicios/destino.service.ts
@@ -15,11 +15,22 @@ export class DestinoService {
   }
 
   /**
-   * Método para enviar datos a un destino específico.
-   * @param endpoint - El endpoint al que se realizará la solicitud POST.
-   * @param data - Datos que se enviarán en el cuerpo de la solicitud.
-   * @returns Una promesa que se resuelve con los datos de la respuesta del servidor.
-   * @throws Error si ocurre un problema durante la solicitud.
+   * Este método realiza una solicitud HTTP POST a un endpoint determinado, enviando los datos proporcionados en el cuerpo de la solicitud.
+   * Tras la respuesta, se recibe los posibles destinos, que se devuelve como parte de los datos de la respuesta.
+   * Si ocurre un error durante la solicitud, se lanza una excepción.
+   *
+   * @param {string} endpoint - El endpoint al que se realizará la solicitud POST.
+   * @param {any} data - Los datos que se enviarán en el cuerpo de la solicitud.
+   * @returns {Promise<any>} Una promesa que se resuelve con los datos de la respuesta del servidor.
+   * @throws {Error} Si ocurre un problema durante la solicitud, como errores de red o del servidor.
+   *
+   * @example
+   * try {
+   *   const data = await this.sendDestinity('/api/endpoint', { key: 'value' });
+   *   console.log(data);
+   * } catch (error) {
+   *   console.error('Error al enviar los datos', error);
+   * }
    */
   async sendDestinity(endpoint: string, data: any): Promise<any> {
     try {
